a1a7c6a18cf3e4678389cff3d7537ca909a7cfcc
Yannick Wiesner <wiesnery@msn.com>
2014-06-23 14:09:58 +0200

    getJSONDescription added

--- bioApi/src/bioSearcher/BSCharacter.java
+++ bioApi/src/bioSearcher/BSCharacter.java
@@ -1,36 +1,62 @@
 package bioSearcher;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 public class BSCharacter extends BSDataStorage {
-	private BSOption[] option = new BSOption[2];
-	
+	private BSOption[] options = new BSOption[2];
+
 	public BSCharacter(int id){
 		super(id);
-		
+
 		switch(id){
-			case 21:
-					this.name = "Size";
-					this.option[0] = new BSOption(31);
-					this.option[1] = new BSOption(32);
-				break;
-			case 22:
-					this.name = "Color";
-					this.option[0] = new BSOption(33);
-					this.option[1] = new BSOption(34);
-				break;
-			case 23:
-					this.name = "Form";
-					this.option[0] = new BSOption(35);
-					this.option[1] = new BSOption(36);
-				break;
-			case 24:
-					this.name = "Habitat";
-					this.option[0] = new BSOption(37);
-					this.option[1] = new BSOption(38);
-				break;
-			default:
-					this.name = "Character undefined.";
-				break;
-			
+		case 21:
+			this.name = "Size";
+			this.options[0] = new BSOption(31);
+			this.options[1] = new BSOption(32);
+			break;
+		case 22:
+			this.name = "Color";
+			this.options[0] = new BSOption(33);
+			this.options[1] = new BSOption(34);
+			break;
+		case 23:
+			this.name = "Form";
+			this.options[0] = new BSOption(35);
+			this.options[1] = new BSOption(36);
+			break;
+		case 24:
+			this.name = "Habitat";
+			this.options[0] = new BSOption(37);
+			this.options[1] = new BSOption(38);
+			break;
+		default:
+			this.name = "Character undefined.";
+			break;
+
 		}
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public String getJSONDescription() {
+		JSONObject json = new JSONObject();
+		json.put("id", this.id);
+		json.put("name", this.id);
+
+		JSONArray JSONoptions = new JSONArray();
+
+		for (BSOption option : options) {
+			JSONObject JSONoption = new JSONObject();
+			JSONoption.put("id", option.id);
+			JSONoption.put("name", option.name);
+
+			JSONoptions.add(JSONoption);
+
+		}
+
+		json.put("options",	JSONoptions);
+
+		return json.toJSONString();
 	}
 }
