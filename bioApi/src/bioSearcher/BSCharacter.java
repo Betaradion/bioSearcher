64ac851f1f15e10c8de4c969d84641171ca070ff
Martin <martinfabianp@hotmail.com>
2014-06-23 13:42:39 +0200

    add BSDataController to Project

--- bioApi/src/bioSearcher/BSCharacter.java
+++ bioApi/src/bioSearcher/BSCharacter.java
@@ -8,27 +8,27 @@ public class BSCharacter extends BSDataStorage {
 		
 		switch(id){
 			case 21:
-					name = "Size";
-					option[0] = new BSOption(31);
-					option[1] = new BSOption(32);
+					this.name = "Size";
+					this.option[0] = new BSOption(31);
+					this.option[1] = new BSOption(32);
 				break;
 			case 22:
-					name = "Color";
-					option[0] = new BSOption(33);
-					option[1] = new BSOption(34);
+					this.name = "Color";
+					this.option[0] = new BSOption(33);
+					this.option[1] = new BSOption(34);
 				break;
 			case 23:
-					name = "Form";
-					option[0] = new BSOption(35);
-					option[1] = new BSOption(36);
+					this.name = "Form";
+					this.option[0] = new BSOption(35);
+					this.option[1] = new BSOption(36);
 				break;
 			case 24:
-					name = "Habitat";
-					option[0] = new BSOption(37);
-					option[1] = new BSOption(38);
+					this.name = "Habitat";
+					this.option[0] = new BSOption(37);
+					this.option[1] = new BSOption(38);
 				break;
 			default:
-					name = "Character undefined.";
+					this.name = "Character undefined.";
 				break;
 			
 		}
