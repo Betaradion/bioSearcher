ab020a3a949de06ea05992ceb3e97732c65436cd
Yannick Wiesner <wiesnery@msn.com>
2014-06-23 14:56:53 +0200

    updated

--- bioApi/src/bioSearcher/BSController.java
+++ bioApi/src/bioSearcher/BSController.java
@@ -8,31 +8,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 
 @WebServlet(description = "Directs to taskspecific classes that handle the data-management", urlPatterns = { "/Families/*" })
 public class BSController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	@SuppressWarnings("unchecked")
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String pathInfo = request.getPathInfo();
-		JSONArray jArray = new JSONArray();
-		JSONObject jObject = new JSONObject();
 
 		if (!pathInfo.isEmpty()) {
 			String[] pathSteps = pathInfo.split("/");
 			if (pathSteps.length <= 1) {
 
 				BSFamilies families = new BSFamilies();
-
-				for (BSFamily family: families.getFamilies()) {
-					jObject.clear();
-					jObject.put(family.getId(), family.getName());
-					jArray.add(jObject.clone());
-				}
-				System.out.print(jArray);
+				System.out.print(families.getJSONDescription());
 			} else {
 				//specific familie_id is given -> handle further action
 				int fId = Integer.parseInt(pathSteps[1]);
@@ -41,7 +30,6 @@ public class BSController extends HttpServlet {
 					if (pathSteps[2].equals("characters")) {
 
 						//Handle Character
-
 						BSFamily family = new BSFamily(fId);
 
 						System.out.print(family.getJSONDescription());
