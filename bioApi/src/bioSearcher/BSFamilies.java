d614bb92b9019086bd8cbadfa54de19378220652
Yannick Wiesner <wiesnery@msn.com>
2014-06-23 13:55:14 +0200

    BSFAmilies refactored

--- bioApi/src/bioSearcher/BSFamilies.java
+++ bioApi/src/bioSearcher/BSFamilies.java
@@ -1,19 +1,17 @@
 package bioSearcher;
 
-public class BSFamilies extends BSDataStorage {
+public class BSFamilies {
 	private BSFamily[] families;
 	
-	public BSFamilies(int id){
-		super(id);
+	public BSFamilies(){
+		families = new BSFamily[2];
 		
-		this.families = new BSFamily[2];
-		
-		this.families[0] = new BSFamily(11);
-		this.families[1] = new BSFamily(12);
+		families[0] = new BSFamily(11);
+		families[1] = new BSFamily(12);
 	}
 	
 	public BSFamily[] getFamilies() {
-		return this.families;
+		return families;
 	}
 
 }
