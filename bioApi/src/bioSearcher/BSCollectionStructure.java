540d23d9e4df3efb1df8d5f0e916a65a0991a7bc
Martin Przibylla <martinfabianp@hotmail.com>
2014-06-24 10:54:19 +0200

    push

---
+++ bioApi/src/bioSearcher/BSCollectionStructure.java
@@ -0,0 +1,22 @@
+package bioSearcher;
+
+public abstract class BSCollectionStructure {
+	private BSDataStructure [] collection;
+	
+	public BSCollectionStructure(){
+		
+	}
+	
+	public abstract String getJSONDescription(int n);
+	
+	public String getJSONDescription(){
+		return getJSONDescription(0);
+	}
+	
+	public abstract void loadSubModules(int n);
+	
+	public void loadSubModules() {
+		loadSubModules(1000);
+	}
+	
+}
