4ca984c62fbc5d369fbff26a2ff064809bc27096
Yannick Wiesner <wiesnery@msn.com>
2014-06-24 11:38:52 +0200

    refactored

--- bioApi/src/bioSearcher/BSOptionCollection.java
+++ bioApi/src/bioSearcher/BSOptionCollection.java
@@ -1,7 +1,5 @@
 package bioSearcher;
 
-import org.json.simple.JSONArray;
-
 public class BSOptionCollection extends BSCollectionStructure {
 	private BSOption[] options = new BSOption[2]; 
 	
@@ -25,24 +23,6 @@ public class BSOptionCollection extends BSCollectionStructure {
 			this.options[0] = new BSOption(37);
 			this.options[1] = new BSOption(38);
 			break;
-		}
-	}
-	
-	
-	@SuppressWarnings("unchecked")
-	@Override
-	public String getJSONDescription(int n) {
-		JSONArray jArray = new JSONArray();
-		for(BSOption option : this.options) {
-			jArray.add(option.getJSONDescription(n-1));
-		}
-		return null;
-	}
-
-	@Override
-	public void loadSubModules(int n) {
-		for (BSOption option : this.options) {
-			option.loadSubmodules(n-1);
 		}
 	}
 }
