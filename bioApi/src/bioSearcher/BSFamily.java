c31a998de855458b59f98f79214a47e5a5fc2a0b
Martin Przibylla <martinfabianp@hotmail.com>
2014-06-24 09:12:09 +0200

    new Class characters.java

--- bioApi/src/bioSearcher/BSFamily.java
+++ bioApi/src/bioSearcher/BSFamily.java
@@ -3,30 +3,14 @@ package bioSearcher;
 import org.json.simple.JSONObject;
 
 public class BSFamily extends BSDataStorage {
-	private BSCharacter[] characters = new BSCharacter[2];
+	private BSCharacters characters;
 
 	public BSFamily(int id){
 		super(id);
-
-		switch(id){
-		case 11:
-			this.name = "Hunde";
-			this.characters[0] = new BSCharacter(21);
-			this.characters[1] = new BSCharacter(22);
-			break;
-		case 12: 
-			name = "Fische";
-			this.characters[0] = new BSCharacter(23);
-			this.characters[1] = new BSCharacter(24);
-			break;
-		default:
-			this.name = "Class undefined";
-			break;
-		}
-
+		characters = new BSCharacters(id);
 	}
 
-	public BSCharacter[] getCharacters() {
+	public BSCharacters getCharacters() {
 		return this.characters;
 	}
 
