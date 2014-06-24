d79d3521955aaf4578481c9e1bfb5aa1b3ae7013
Yannick Wiesner <wiesnery@msn.com>
2014-06-24 10:05:15 +0200

    reverted

--- bioApi/src/bioSearcher/BSFamily.java
+++ bioApi/src/bioSearcher/BSFamily.java
@@ -3,14 +3,30 @@ package bioSearcher;
 import org.json.simple.JSONObject;
 
 public class BSFamily extends BSDataStorage {
-	private BSCharacters characters;
+	private BSCharacter[] characters = new BSCharacter[2];
 
 	public BSFamily(int id){
 		super(id);
-		characters = new BSCharacters(id);
+
+		switch(id){
+		case 11:
+			this.name = "Hunde";
+			this.characters[0] = new BSCharacter(21);
+			this.characters[1] = new BSCharacter(22);
+			break;
+		case 12: 
+			name = "Fische";
+			this.characters[0] = new BSCharacter(23);
+			this.characters[1] = new BSCharacter(24);
+			break;
+		default:
+			this.name = "Class undefined";
+			break;
+		}
+
 	}
 
-	public BSCharacters getCharacters() {
+	public BSCharacter[] getCharacters() {
 		return this.characters;
 	}
 
