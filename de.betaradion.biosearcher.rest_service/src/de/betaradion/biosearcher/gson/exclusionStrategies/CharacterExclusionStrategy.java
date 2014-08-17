60f94346e4be6026cc13c3b22b48a7c00b10908a
Yannick Wiesner <wiesnery@msn.com>
2014-08-17 15:28:25 +0200

    exclusion

---
+++ de.betaradion.biosearcher.rest_service/src/de/betaradion/biosearcher/gson/exclusionStrategies/CharacterExclusionStrategy.java
@@ -0,0 +1,22 @@
+package de.betaradion.biosearcher.gson.exclusionStrategies;
+
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+
+public class CharacterExclusionStrategy implements ExclusionStrategy {
+
+	@Override
+	public boolean shouldSkipClass(Class<?> arg0) {
+		return false;
+	}
+
+	@Override
+	public boolean shouldSkipField(FieldAttributes att) {
+		if (att.getName().equals("family") || att.getName().equals("character")
+				|| att.getName().equals("matchTables")) {
+			return true;
+		}
+		return false;
+	}
+
+}
